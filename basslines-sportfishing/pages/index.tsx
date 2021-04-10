e21d4cc7e74f5312381055a103a3e2342deb9ae0
Trent325 <trgreen2002@gmail.com>
2021-04-09 21:45:25 -0400

    third committ

---
+++ basslines-sportfishing/pages/index.tsx
@@ -0,0 +1,16 @@
+import {NextPage} from "next";
+import style from "/style/Home.module.scss";
+import Header from "../components/Header";
+
+const Home: NextPage =() => {
+  return (
+
+    <div>
+      <Header/>
+      hello
+    </div>
+    
+  )
+};
+
+export default Home;
