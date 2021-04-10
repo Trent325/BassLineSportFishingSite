13524cfb6bc576d516f416aaf8cc36b475e6ab70
Trent325 <trgreen2002@gmail.com>
2021-04-09 21:35:47 -0400

    second commit

---
+++ basslines-sportfishing/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Header.module.scss';
+
+const Header: React.FC =() =>{
+    return(
+        <div className={style.headerParent}>
+
+        <div id = "header"></div>
+    )
+};
+
+export default Header;
