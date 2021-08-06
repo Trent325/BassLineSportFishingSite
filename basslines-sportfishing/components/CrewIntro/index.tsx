25f4061181d9846f9681e0245608319f9b08a4b7
Trent Green <70169861+Trent325@users.noreply.github.com>
2021-08-06 13:39:18 -0400

    Update index.tsx
    fixed a typo

--- basslines-sportfishing/components/CrewIntro/index.tsx
+++ basslines-sportfishing/components/CrewIntro/index.tsx
@@ -21,7 +21,7 @@ const CrewIntro: React.FC =() => {
 
             <div className = {style.parent}>
                 <div className = {style.Box}>
-                Captain Bill and his son Captain Willy have been fishing all over the coast of New Jersey for the entirety of both of their lives. They are a perfect example of the saying ‘the apple does far from the tree’ Bill and his son both share a passion for fishing. It does not matter if you are an avid angler or are looking to get on the water for some family fun, they got it covered.
+                Captain Bill and his son Captain Willy have been fishing all over the coast of New Jersey for the entirety of both of their lives. They are a perfect example of the saying ‘the apple does not fall far from the tree’ Bill and his son both share a passion for fishing. It does not matter if you are an avid angler or are looking to get on the water for some family fun, they got it covered.
                 </div>
 
             </div>
